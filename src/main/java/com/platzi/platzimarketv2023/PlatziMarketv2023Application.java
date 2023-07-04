daa06fa598319c9bae719a0d265c0e369cba5da0
orlychaparro <orlychaparro@gmail.com>
2023-07-04 12:08:55 -0400

    Proyecto incial platzi market v 2023

---
+++ src/main/java/com/platzi/platzimarketv2023/PlatziMarketv2023Application.java
@@ -0,0 +1,13 @@
+package com.platzi.platzimarketv2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketv2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketv2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/platzimarketv2023/PlatziMarketv2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.platzimarketv2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketv2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
